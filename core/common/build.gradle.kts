01ad2abc7864829dcd7b935b6e58be4044f09898
markmelnarowycz <mark.melnarowych@gubadoo.app>
2024-08-21 14:54:00 +0300

    fix File not Found local.properties in gradle common module

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -6,15 +6,17 @@ plugins {
 
 val apikeyPropertiesFile = rootProject.file("local.properties")
 val apikeyProperties = Properties().apply {
-    load(apikeyPropertiesFile.inputStream())
+    if (apikeyPropertiesFile.isFile){
+        load(apikeyPropertiesFile.inputStream())
+    }
 }
 
 android {
     defaultConfig {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField("String", "BASE_URL", (apikeyProperties["BASE_URL"] as? String) ?: System.getenv("BASE_URL"))
-        buildConfigField("String", "API_KEY", (apikeyProperties["API_KEY"] as? String) ?: System.getenv("API_KEY"))
+        buildConfigField("String", "BASE_URL", (apikeyProperties["BASE_URL"] as? String) ?:  System.getenv("BASE_URL"))
+        buildConfigField("String", "API_KEY", (apikeyProperties["API_KEY"] as? String) ?:  System.getenv("API_KEY"))
     }
 
     buildFeatures.buildConfig = true
